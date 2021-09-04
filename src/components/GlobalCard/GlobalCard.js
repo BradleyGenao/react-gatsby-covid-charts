5c23992fdc5ba1b18c1b8b0ee71694abd56d0c6c
Bradley Genao <bjgenao@gmail.com>
2021-09-04 12:46:18 -0700

    Fixed the card not working on large screen issue

--- src/components/GlobalCard/GlobalCard.js
+++ src/components/GlobalCard/GlobalCard.js
@@ -1,25 +1,24 @@
 import React from "react"
 import styled from "styled-components"
-import Video from '../../assets/videos/card-bg.mp4'
+import Video from "../../assets/videos/card-bg.mp4"
 
 const GlobalCard = () => {
   return (
     <CardContainer>
       <CardBg>
-        <VideoBg src={Video}  type="video/mp4" autoPlay loop muted playsInline />
+        <VideoBg src={Video} type="video/mp4" autoPlay loop muted playsInline />
         <CardContent>
           <CardItem>
-            <CardHeading>Global</CardHeading>
+            <CardHeading>
+              <h1>Global</h1>
+            </CardHeading>
 
-            <div>
+            <CardDataContainer>
               <h3>Cases</h3>
               <p>100,000,000,000</p>
-            </div>
-
-            <div>
               <h3>Deaths </h3>
               <p>100,000</p>
-            </div>
+            </CardDataContainer>
           </CardItem>
         </CardContent>
       </CardBg>
@@ -30,23 +29,34 @@ const GlobalCard = () => {
 export default GlobalCard
 
 const CardContainer = styled.div`
-position: relative;
+  position: relative;
+  display: flex;
 `
+
+
+
+
+const CardDataContainer = styled.div`
+  color: white;
+  margin: 15px;
+`
+
 
 const CardBg = styled.div`
   position: relative;
   overflow: hidden;
   width: 100%;
-  height: 470px;
+  height: 100%;
+  margin-top: -75px;
 `
 
 const VideoBg = styled.video`
   z-index: 0;
-  max-width: 100%;
-  height: auto;
 
-
-
+  width: 100%;
+  height: 100%;
+  -o-object-fit: cover;
+  object-fit: cover;
 `
 
 const CardItem = styled.div`
@@ -56,8 +66,19 @@ const CardItem = styled.div`
   z-index: 1;
 `
 
-const CardHeading = styled.h1`
-color: white;
+const CardHeading = styled.div`
+  color: white;
+  font-size: clamp(1.5rem, 6vw, 3.6rem);
+  margin-bottom: 1.5rem;
+  letter-spacing: 3px;
+  padding: 0 1rem;
+
+  h1 {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    text-align: center;
+  }
 `
 
 const CardContent = styled.div``
