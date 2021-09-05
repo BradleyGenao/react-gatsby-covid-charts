c574b44332de281a5a3699b24dba3375e78e9265
Bradley Genao <bjgenao@gmail.com>
2021-09-05 14:32:30 -0700

    Fixed query graphql bug

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { graphql, useStaticQuery } from "gatsby"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import { Hero, Stats, GlobalCard, DailyStats } from "../components"
 import Seo from "../components/seo"
@@ -8,7 +8,7 @@ const IndexPage = ({ data }) => {
 
 
 
-  
+
   return (
     <Layout>
       <Seo title="Home" />
@@ -25,49 +25,41 @@ const IndexPage = ({ data }) => {
   )
 }
 
-
 export const homePageQuery = graphql`
-  query MainQuery {
-
+  query MyQuery {
+    covidGlobal {
+      confirmed {
+        value
+      }
+      deaths {
+        value
+      }
+      lastUpdate(formatString: "MMMM DD, YYYY hh:mm A")
+    }
     covidDaily {
-      data {
-        reportDate
-        confirmed {
-          total
-        }
+      daily {
+        totalConfirmed
         deaths {
           total
         }
-        active
+        reportDate(formatString: "MMMM DD, YYYY hh:mm A")
       }
     }
-    countiresJson {
-      name
+    countries {
+      countries {
+        confirmed
+        deaths
+        countryRegion
+        lastUpdate
+        lat
+        long
+        provinceState
+        iso2
+      }
+    }
+    countiresListJson {
       code
-    }
-    countriesConfirmed {
-      data {
-        uid
-        confirmed
-        deaths
-        iso2
-        lastUpdate
-        lat
-        long
-        countryRegion
-      }
-    }
-    countriesDeath {
-      data {
-        uid
-        confirmed
-        deaths
-        iso2
-        lastUpdate
-        lat
-        long
-        countryRegion
-      }
+      name
     }
   }
 `
